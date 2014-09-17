995d894ef4824f394dbd43125e38c1d4aa04d1f2
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-09-17 19:29:01 +1100

    Int

---
+++ Urok04/Urok04/Urok04.cpp
@@ -0,0 +1,18 @@
+// Urok04.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{ 
+	int x;
+	x=getchar(); 
+	putchar(x);
+	int y;
+	y=getchar();
+	putchar(y);
+
+	return 0;
+}
+
